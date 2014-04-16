d090a6d14ff49387e9c443f97710e48a6c9a4bdb
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-04-16 10:33:00 -0400

    changed to match naming conventions

--- app.rb
+++ app.rb
@@ -3,15 +3,16 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:///good_books.db"
 
-post "/add_book" do
+post "/new" do
 	good_book = GoodBooks.new
+	
 	good_book.title = params[:title]
 	good_book.author = params[:author]
 	good_book.save
-	redirect :"/main"
+	redirect :"/index"
 end
 
-get "/main" do 
+get "/index" do 
  	@good_books = GoodBooks.all
  	erb :"good_books/index"
 end
@@ -24,7 +25,7 @@ get "/new" do
 	erb :"good_books/new"
 end
 
-get"/good_books/:id/delete" do
+get"/good_books/:id/show" do
 	@good_books = GoodBooks.find(params[:id])
 	 erb :"good_books/delete"
 	end
@@ -38,9 +39,7 @@ get "/good_books/:id/edit" do
 put '/good_books/:id/edit' do
 	good_book = GoodBooks.find params[:id]
 	good_book.update_attributes(params[:good_book])
-
-	
-	redirect :"/main"
+	redirect :"/index"
 end
 
  
@@ -49,7 +48,7 @@ end
 delete '/good_books/:id/delete' do
 	@good_books =GoodBooks.find params[:id]
 	@good_books.destroy
-		redirect :"/main"
+		redirect :"/index"
 end
 
 
